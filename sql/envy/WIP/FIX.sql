ae6ffd49d4052e1a3f4ff3f41e017860ff8cf81e
Envy602 <lionel.cuffel@sfr.fr>
2012-04-05 15:54:54 +0200

    Add WIP Sql for new Ulduar (need fix in conditions and DB).

---
+++ sql/envy/WIP/FIX.sql
@@ -0,0 +1,5 @@
+-- THORIM - Charge Orb
+DELETE FROM conditions WHERE SourceEntry = 62016;
+INSERT INTO `conditions` VALUES
+('13','0','62016','0','18','1','33378','0','0','',NULL);
+UPDATE `creature_template` SET `unit_flags` = 33685508 WHERE `entry` = 33378;
