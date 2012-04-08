7c84a50edaf31a62d2331d7f6f36f13f2c56dcf5
Envy602 <lionel.cuffel@sfr.fr>
2012-04-08 16:36:53 +0200

    Fix typo

--- sql/envy/world/2012_03_11_01_world_gunship.sql
+++ sql/envy/world/2012_03_11_01_world_gunship.sql
@@ -330,8 +330,7 @@ UPDATE item_template SET ScriptName = 'item_icc_rocket_pack' WHERE entry = 49278
 UPDATE `creature_template` SET `npcflag`=1 WHERE `entry`=37184;
 
 -- Fix DB errors conditions
-DELETE FROM `conditions` WHERE `SourceEntry` IN (69705, 69400, 69402, 70175, 70374, 70383);
-DELETE FROM `conditions` WHERE `SourceEntry` IN (72959, 70173);
+DELETE FROM `conditions` WHERE `SourceEntry` IN (69705, 69400, 69402, 70175, 70374, 70383, 72959, 70173);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (13, 1, 69705, 0, 0, 31, 0, 3, 36838, 0, 0, 0, '', NULL),
 (13, 1, 69705, 0, 1, 31, 0, 3, 36839, 0, 0, 0, '', NULL),
